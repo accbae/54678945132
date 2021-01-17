ba25aef9185824dfdc011360417994fc2f3318cc
accbae <77581574+accbae@users.noreply.github.com>
2021-01-17 17:16:34 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<?
-
-
-$content = '5,
-			                    
-			                    Mill Dale Court<br/>
-			                    Drighlington<br/>
-			                    
-				                
-				                    Bradford<br/>
-			                        West Yorkshire, BD11 1DE';
-			                        
-			                        
-$myArray = preg_split('/<br[^>]*>/i', $content);
-
-print_r($myArray);			                        
-			                        
-?>
+<?php echo 'blablabla';?>
